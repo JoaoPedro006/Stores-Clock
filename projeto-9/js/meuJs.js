e7a5b330ce9e285d7148e50a66b0724c71f6f08f
JoaoPedro006 <jpprofessionalit@gmail.com>
2023-12-29 19:34:49 -0300

    meu projeto

---
+++ projeto-9/js/meuJs.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.link').click(function(){
+        location.href = "https://www.linkedin.com/in/jo%C3%A3o-pedro-maia-pinheiro-23532a269/"
+    });
+
+  $('.git').click(function(){
+    location.href = "https://github.com/JoaoPedro006?tab=repositories/"
+    });
+
+
+});
